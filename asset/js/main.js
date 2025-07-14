18e5ec6aefaaf6b7b442819141137b4bda534177
NadaNasr22 <nadanasr616@gmail.com>
2025-07-14 21:30:24 +0200

    update index file

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,112 +1,15 @@
-/*let user = {
+const accordion = document.getElementById("accordion");
 
-
-
-    firstname: "nada",
-    lastname: "nasr",
-
-    adress: {
-
-        eg: "gize",
-        arb: "ridy",
-        ksa: "call",
-
-        getmaindress: function() {
-            return `get your dress sit ${user.adress.arb}`;
-        },
-
-    }
-    // getfullname: function() {
-
-    // return `full name :${user.firstname} ${user.lastname}`;
-    ///}
-};
-
-
-console.log(user.firstname);
-console.log(user.lastname);
-console.log(user.adress.eg);
-
-console.log(user.adress.getmaindress());
-console.log(user.getfullname());*/
-
-/*
-let maindekawin = {
-    hasdiskawin: true,
-
-    show: function() {
-        return ` you ${this.hasdiskawin ? "":"don't" } have disqawind`;
-    },
-
-
-};
-console.log(maindekawin.hasdiskawin);
-console.log(maindekawin.show());
-
-let other = object.create(maindekawin);
-other.hasdiskawin = false;
-console.log(other.hasdiskawin);
-console.log(other.show());*/
-
-/*let myobject1 = {
-    prop1: "valou1",
-    prop2: "valou2",
-
-
-
-    method1: function() {
-        return `"valou11"`;
-    }
-
-}
-let myobject2 = {
-    prop3: "valou3",
-    prop4: "valou4",
-
-    method2: function() {
-        return `"valou22"`;
-    }
-
-}
-let target = {
-    prop5: "valou5",
-}
-
-Object.assign(target)
-
-console.log(target)
-
-const out = Object.assign({}, target, { prop7: "valou7" });
-console.log(out);
-console.log(myobject2.method2())*/
-
-
-
-
-/*function Phone(serual, calar, price) {
-
-    this.serual = serual;
-    this.calar = calar;
-    this.price = price;
-}
-
-let Phone1 = new Phone(111, "red", 454);
-let Phone2 = new Phone(562, "blue", 600);
-let Phone3 = new Phone(246, "black", 500);
-
-console.log(Phone1.serual);
-console.log(Phone1.calar);
-console.log(Phone1.price);
-
-console.log(Phone2.serual);
-console.log(Phone2.calar);
-console.log(Phone2.price);
-
-console.log(Phone3.serual);
-console.log(Phone3.calar);
-console.log(Phone3.price);*/
-
-/*window.addEventListener('DOMContentLoaded', function() {
-    const circle = document.querySelector('.my-circle');
-    circle.setAttribute('cy', '400')
+/*accordion.forEach((btn) => {
+    btn.addEventListener("click", function() {
+        btn.textContent = btn.accordion === "-" ? "+" : "-";
+    })
 });*/
+const currentText = accordion.textContent.trim();
+accordion.addEventListener('click', function() {
+    if (currentText.startsWith("-")) {
+        accordion.textContent = ("+").currentText.slice(1).trim();
+    } else {
+        accordion.textContent = ("-").currentText.slice(1).trim();
+    }
+})
